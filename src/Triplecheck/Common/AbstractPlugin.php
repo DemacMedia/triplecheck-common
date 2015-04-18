a571542827f2a536622e5f4c116a2ddad192ef1a
lfolco <me@laurafolco.com>
2015-04-18 16:54:21 -0700

    added shell wrapper

--- src/Triplecheck/Common/AbstractPlugin.php
+++ src/Triplecheck/Common/AbstractPlugin.php
@@ -2,6 +2,18 @@
 
 namespace Triplecheck\Common;
 
+use AdamBrett\ShellWrapper\Runners\Exec as Exec;
+use AdamBrett\ShellWrapper\Command\Builder as CommandBuilder;
+
 abstract class AbstractPlugin
 {
+
+    public $_command;
+
+    public $_pluginCode;
+
+    public function __construct()
+    {
+        $this->_command = new CommandBuilder($this->_pluginCode);
+    }
 }
