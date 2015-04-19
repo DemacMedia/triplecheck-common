194100661509f69aece67d517178a837dd1509cb
Allan MacGregor <amacgregor@allanmacgregor.com>
2015-04-19 08:04:41 -0700

    Update static class phpdocblocks

--- src/Triplecheck/Triplecheck.php
+++ src/Triplecheck/Triplecheck.php
@@ -4,10 +4,41 @@ namespace Triplecheck;
 
 use Triplecheck\Common\PluginFactory;
 
+/**
+ * Class: Triplecheck
+ *
+ * Provides static access to the plugin factory methods. This is
+ * the recommended route for plugin instantiation.
+ *
+ * Example:
+ *
+ * // Create a plugin for PhpCodeSniffer
+ *
+ * $plugin = Triplecheck::create('Phpcs');
+ *
+ * // Configure the plugin
+ * $options = [...];
+ * $plugin->configure($options);
+ *
+ * // Run the Plugin
+ * $rawData = $plugin->run();
+ * 
+ * // Get the score for this plugin
+ * pending
+ *
+ * @see Triplecheck\Common\PluginFactory
+ */
 class Triplecheck
 {
+    /**
+     * Factory static class property, used to hold Plugin factory instance 
+     */
     public static $factory;
 
+    /**
+     * getFactory
+     *
+     */
     public function getFactory()
     {
         if(is_null(static::$factory))
